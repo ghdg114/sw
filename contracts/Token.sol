f64c406d1ae8434de819f9b28264ac2b5cece00d
ghdg114 <162067388+ghdg114@users.noreply.github.com>
2024-08-23 22:58:49 +0000

    feat: initiated the project

---
+++ contracts/Token.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TestToken is ERC20 {
+    constructor()ERC20("46c45a11b7aa1cc9d42d7764a287802f02836ab70cfe9d5e9be3093ef904b75f","k"){} 
+
+    function mint100tokens() public {
+        _mint(msg.sender, 100*10**18);
+    }
+
+    function burn100tokens() public{
+        _burn(msg.sender, 100*10**18);
+    }
+}
